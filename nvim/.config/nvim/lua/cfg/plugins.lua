05a272fe47e736bb8beb8aa233848e0ce06c0ec2
Gabriel Kakizaki <gkakizaki@gmail.com>
2024-07-05 19:55:46 -0300

    feat(nvim): autoload only git sessions

--- nvim/.config/nvim/lua/cfg/plugins.lua
+++ nvim/.config/nvim/lua/cfg/plugins.lua
@@ -89,7 +89,12 @@ local plugins = {
   },
   {
     'Shatur/neovim-session-manager',
-    opts = {},
+    config = function()
+      local config = require('session_manager.config')
+      require('session_manager').setup({
+        autoload_mode = config.AutoloadMode.GitSession,
+      })
+    end
   },
   'hrsh7th/nvim-cmp',
   'hrsh7th/cmp-nvim-lsp',
