d7cd11e8cba7410cd4c8a1d93b432adca6743354
Gabriel Kakizaki <gkakizaki@gmail.com>
2024-10-05 07:54:48 -0300

    fix(nvim): warning about gitsigns deprecation

--- nvim/.config/nvim/lua/cfg/gitsigns.lua
+++ nvim/.config/nvim/lua/cfg/gitsigns.lua
@@ -13,9 +13,6 @@ require('gitsigns').setup {
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
   },
-  current_line_blame_formatter_opts = {
-    relative_time = false
-  },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
